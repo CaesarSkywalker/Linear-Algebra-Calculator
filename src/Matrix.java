fb73ce6c8989901e8f85d6886135f15c5060a741
Caesar Li <caesarli88@berkeley.edu>
2024-05-15 01:05:05 -0700

    determinant

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,29 +1,76 @@
 public class Matrix {
-    private double[][] matrix;
-    private int width;
-    private int height;
-    public Matrix(int m, int n){
-        matrix = new double[m][n];
-        width = m;
-        height = n;
-    }
-    public Matrix(double[][] matrix) {
-        this.matrix = matrix;
-        width = matrix.length;
-        height = matrix[0].length;
-    }
-    public double[] getCol(int i) {
-        return matrix[i];
-    }
-    public double[] getRow(int i) {
-        double[] row = new double[width];
-        int index = 0;
-        for (double[] col: matrix) {
-            row[index] = col[i];
-            index++;
-        }
-        return row;
+    private final double[][] matrix;
+    private final int numRows;
+    private final int numCols;
+    private Double det;
+
+    public Matrix(int numRows, int numCols) {
+        this.matrix = new double[numRows][numCols];
+        this.numRows = numRows;
+        this.numCols = numCols;
     }
 
+    public Matrix(double[][] matrix) {
+        this.matrix = matrix;
+        this.numRows = matrix.length;
+        this.numCols = matrix[0].length;
+    }
+
+    public double[] getCol(int colIndex) {
+        double[] col = new double[numRows];
+        for (int i = 0; i < numRows; i++) {
+            col[i] = matrix[i][colIndex];
+        }
+        return col;
+    }
+    public double[] getRow(int rowIndex) {
+        return matrix[rowIndex];
+    }
+
+    public Matrix transpose() {
+        double[][] transposedMatrix = new double[numCols][numRows];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return new Matrix(transposedMatrix);
+    }
+    public double determinant() {
+        if (numRows != numCols) {
+            throw new IllegalStateException("Determinant is not defined for non-square matrices");
+        }
+        if (det != null) {
+            return det;
+        }
+        return calculateDeterminant(matrix);
+    }
+
+    private double calculateDeterminant(double[][] matrix) {
+        int n = matrix.length;
+        if (n == 1) {
+            return matrix[0][0];
+        } else if (n == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        }
+
+        double determinant = 0;
+
+        for (int i = 0; i < n; i++) {
+            double[][] submatrix = new double[n-1][n-1];
+            for (int row = 1; row < n; row++) {
+                for (int col = 0; col < n; col++) {
+                    if (col < i) {
+                        submatrix[row-1][col] = matrix[row][col];
+                    } else if (col > i) {
+                        submatrix[row-1][col-1] = matrix[row][col];
+                    }
+                }
+            }
+            determinant += matrix[0][i] * Math.pow(-1, i) * calculateDeterminant(submatrix);
+        }
+        det = determinant;
+        return determinant;
+    }
 
 }
